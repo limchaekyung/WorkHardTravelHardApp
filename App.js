e8edd6a9fd8652f9276c1fb61e97679015bbc2e1
limchaekyung <corud505@naver.com>
2022-03-02 23:44:02 +0900

    Paint To Dos

--- App.js
+++ App.js
@@ -1,28 +1,96 @@
 import { StatusBar } from "expo-status-bar";
+import React, { useState,useEffect } from "react";
 import {
   StyleSheet,
   Text,
   View,
-  TouchableOpcity,
-  TouchableHighlight,
-  TouchableWithoutFeedback,
-  Pressable
+  TouchableOpacity,
+  TextInput,
+  ScrollView,
+  AsyncStorage,
 } from "react-native";
+import { theme } from "./colors";
+
+const STORAGE_KEY = "@toDos"
 
 export default function App() {
+  const [working, setWorking] = useState(true);
+  const [text, setText] = useState("");
+  const [toDos, setToDos] = useState({});
+  useEffect(() => {
+    loadToDos();
+  }, [])
+
+  const travel = () => setWorking(false);
+  const work = () => setWorking(true);
+  const onChangeText = (payload) => setText(payload);
+  const saveToDos = async(toSave) => {
+    try{
+      await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(toSave));  
+    }catch(e){
+      throw e;
+    }
+  }
+  const loadToDos = async() => {
+    const s = await AsyncStorage.getItem(STORAGE_KEY);
+    setToDos(JSON.parse(s));
+  }
+  const addToDo = async() => {
+    if (text === "") {
+      return;
+    }
+    // save to do
+    /* const newToDos = Object.assign({}, toDos, {
+      [Date.now()]: { text, work: working },
+    }); */
+    const newToDos = { ...toDos, [Date.now()]: { text, working } };
+    // 이전 object를 가진 새로운 object 생성
+    setToDos(newToDos);
+    await saveToDos(newToDos);
+    setText("");
+  };
+  console.log(toDos);
   return (
     <View style={styles.container}>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+
       <View style={styles.header}>
-        <TouchableOpcity>
-          <Text style={styles.btnText}>Work</Text>
-        </TouchableOpcity>
-        <Pressable
-          onPress={() => console.log("pressed")}
-        >
-          <Text style={styles.btnText}>Travel</Text>
-        </Pressable>
+        <TouchableOpacity onPress={work}>
+          <Text
+            style={{ ...styles.btnText, color: working ? "white" : theme.grey }}
+          >
+            Work
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={travel}>
+          <Text
+            style={{
+              ...styles.btnText,
+              color: !working ? "white" : theme.grey,
+            }}
+          >
+            Travel
+          </Text>
+        </TouchableOpacity>
       </View>
+      <TextInput
+        onSubmitEditing={addToDo}
+        returnKeyType="go"
+        onChangeText={onChangeText}
+        value={text}
+        style={styles.input}
+        placeholder={working ? "Add a To Do" : "Where do you wan to go?"}
+      />
+      <ScrollView>
+        {Object.keys(toDos).map((key) =>
+          toDos[key].working === working ? (
+            <View style={styles.toDo} key={key}>
+              <Text style={styles.toDoText}>{toDos[key].text}</Text>
+              <Text>❌</Text>
+            </View>
+          ) : null
+        )}
+      </ScrollView>
     </View>
   );
 }
@@ -30,7 +98,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#000",
+    backgroundColor: theme.bg,
     paddingHorizontal: 20,
   },
   header: {
@@ -42,5 +110,28 @@ const styles = StyleSheet.create({
     fontSize: 38,
     fontWeight: "600",
     color: "white",
+  },
+  input: {
+    backgroundColor: "white",
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+    marginVertical: 20,
+    fontSize: 18,
+  },
+  toDo: {
+    backgroundColor: theme.grey,
+    marginBottom: 10,
+    paddingVertical: 20,
+    paddingHorizontal: 40,
+    borderRadius: 15,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between"
+  },
+  toDoText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "500",
   },
 });
